e79d0a8e07f7a5f20aa9abe1b6a9fa20ca0d97ea
marc115 <marchanoth@protonmail.com>
2024-12-16 21:23:12 -0500

    base project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        heading: '4rem'
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
